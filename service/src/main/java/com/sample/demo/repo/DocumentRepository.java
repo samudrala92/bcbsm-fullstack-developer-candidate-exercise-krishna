162be4cacdb83de1a63f9f970dbfd0a7a2102f3f
samudrala92 <141285398+samudrala92@users.noreply.github.com>
2023-08-03 22:24:02 -0400

    removed files

--- service/src/main/java/com/sample/demo/repo/DocumentRepository.java
+++ service/src/main/java/com/sample/demo/repo/DocumentRepository.java
@@ -2,6 +2,7 @@ package com.sample.demo.repo;
 
 import com.sample.demo.model.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface DocumentRepository extends MongoRepository<Document, String> {
 
